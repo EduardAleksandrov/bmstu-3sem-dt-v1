c13f4ae5f0d8946136453d371c790912b588c54a
Eduard Aleksandrov <eduard.ale@list.ru>
2023-11-18 23:48:00 +0300

    Лаба 8: бинарный поиск + тест 3

--- 8.1.1.bin_poisk.cpp
+++ 8.1.1.bin_poisk.cpp
@@ -1,6 +1,7 @@
 /*
     Task: Реализация бинарного поиска
-    тест - готово
+    тест1
+    добавлен тест2 - работает
 */
 
 #include <iostream>
@@ -11,14 +12,13 @@ using namespace std;
 
 int search(vector <int>&, int);
 void test(int, int, vector <int>&);
+bool test_two(int, int, vector <int>&);
 
 
 int main()
 {
     
     vector <int> mass_one {-3,1,2,3,4,8,12,14};
-    vector <int> mass_two {-3};
-
     int search_elem {-3};
 
     // int result {-1};
@@ -26,8 +26,26 @@ int main()
     // if(result == -1) cout << "Элемент не найден" << endl;
     // else cout << "Индекс равен: " << result << endl;
     
-
+// test 1
     test(-5, 18, mass_one);
+// test 2
+    vector <int> mass_two {};
+    vector <int> mass_three {-3};
+    vector <int> mass_four {-3,4};
+    vector <int> mass_five {0,1,3,5,7,9,11};
+    vector <int> mass_six {2,4,6,8,10,12,14,16,18,20,22,24,26,28};
+
+
+    int ids_size = 6;
+    vector <int> *ids[] = {&mass_one, &mass_two, &mass_three, &mass_four, &mass_five, &mass_six};
+    for(int i = 0; i < ids_size; i++)
+    {
+        int res {-1};
+        res = test_two(-5, 30, *ids[i]);
+        if(res == 0) cout << "Тест 2 пройден" << endl;
+        else cout << "Тест 2 не пройден" << endl;
+    }
+    
 
     return 0;
 }
@@ -43,6 +61,19 @@ void test(int fv, int lv, vector <int>& mass)
         if(result == -1) cout << "не найден" << endl;
         else cout << result << endl;
     }
+    
+}
+bool test_two(int fv, int lv, vector <int>& mass)
+{
+    int result {-1};
+    int res_num {0};
+    for(int i = fv; i <=lv; i++)
+    {
+        result = search(mass, i);
+        if(result != -1) res_num++;
+    }
+    if(res_num == mass.size()) return 0;
+    return 1;
     
 }
 
