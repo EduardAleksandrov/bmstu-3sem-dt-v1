108f1350906a95a3a1531cb32f9f723c3a900697
Eduard Aleksandrov <eduard.ale@list.ru>
2023-11-19 22:36:42 +0300

    Лаба 8: бинарный поиск + тест 6

--- 8.1.2.bin_poisk.cpp
+++ 8.1.2.bin_poisk.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-int search(vector <int>&, int);
+int search(vector <int>&, int, int&);
 void test(int, int, vector <int>&);
 bool test_two(int, int, vector <int>&);
 
@@ -20,15 +20,16 @@ int main()
 {
     
     vector <int> mass_one {-3,1,2,3,4,8,12,14};
-    int search_elem {-3};
+    int search_elem {3};
 
-    // int result {-1};
-    // result = search(mass_two, search_elem);
-    // if(result == -1) cout << "Элемент не найден" << endl;
-    // else cout << "Индекс равен: " << result << endl;
+    int steps {0};
+    int result {-1};
+    result = search(mass_one, search_elem, steps);
+    if(result == -1) cout << "Элемент не найден" << endl;
+    else cout << "Индекс равен: " << result << ", шагов: " << steps << endl;
     
 // test 1
-    test(-5, 18, mass_one);
+    // test(-5, 18, mass_one);
 // test 2
     vector <int> mass_two {};
     vector <int> mass_three {-3};
@@ -55,9 +56,10 @@ int main()
 void test(int fv, int lv, vector <int>& mass)
 {
     int result {-1};
+    int steps {0};
     for(int i = fv; i <=lv; i++)
     {
-        result = search(mass, i);
+        result = search(mass, i, steps);
 
         cout << "i = "<< i << ", индекс = ";
         if(result == -1) cout << "не найден" << endl;
@@ -69,9 +71,10 @@ bool test_two(int fv, int lv, vector <int>& mass)
 {
     int result {-1};
     int res_num {0};
+    int steps {0};
     for(int i = fv; i <=lv; i++)
     {
-        result = search(mass, i);
+        result = search(mass, i, steps);
         if(result != -1) res_num++;
     }
     if(res_num == mass.size()) return 0;
@@ -79,16 +82,18 @@ bool test_two(int fv, int lv, vector <int>& mass)
     
 }
 
-int search(vector <int>& mass, int search_elem)
+int search(vector <int>& mass, int search_elem, int& steps)
 {
     if(mass.size() == 0) return -1;
     int L {0};
     int size = mass.size()-1;
     int R {size};
+    steps = 0;
     
     int mid_ind {0};
     while(L <= R)
     {
+        steps++;
         mid_ind = (L + R) / 2;
         if(search_elem < mass[mid_ind]) R = mid_ind - 1;
         else if(search_elem > mass[mid_ind]) L = mid_ind + 1;
